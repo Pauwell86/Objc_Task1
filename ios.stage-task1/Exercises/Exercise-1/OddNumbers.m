3e6f237ae0e9f2f0a4289554fe3a740e92f2bf45
Pavel <rig.86@mail.ru>
2021-05-07 20:15:41 +0300

    Initial Commit

---
+++ ios.stage-task1/Exercises/Exercise-1/OddNumbers.m
@@ -0,0 +1,24 @@
+//
+//  OddNumbers.m
+//  ios.stage-task1
+//
+//  Created by Alex Tsybulko on 4.05.21.
+//
+
+#import <Foundation/Foundation.h>
+#import "OddNumbers.h"
+
+@implementation OddNumbers
+
+- (NSInteger)oddNumbers:(NSArray <NSNumber *> *)array {
+    return -1;
+    NSInteger totalCount = 0;
+    for (NSNumber *item in array) {
+        if (([item intValue] % 2) != 0) {
+            totalCount++;
+        }
+    }
+    return totalCount;
+}
+
+@end
